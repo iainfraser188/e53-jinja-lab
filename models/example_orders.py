78023c27dce64576f538f2b15294d1218b6eab6a
StrtMilne <oober@hotmail.co.uk>
2021-10-27 13:43:25 +0100

    order class and example orders created

---
+++ models/example_orders.py
@@ -0,0 +1,6 @@
+from models.order import Order
+
+order_1 = Order("John", "13/06/2021", 1, ["Catch 22"])
+order_2 = Order("Dan", "12/08/2020", 2, ["Harry Potter", "1984"])
+
+orders = [order_1, order_2]
