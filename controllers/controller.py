51afd77fa0dd3e82ee0d8b4f92b74243e2ab673f
StrtMilne <oober@hotmail.co.uk>
2021-10-27 14:44:52 +0100

    finished first extension

--- controllers/controller.py
+++ controllers/controller.py
@@ -6,3 +6,9 @@ from models.example_orders import orders
 @shop_app.route('/orders')
 def index():
     return render_template('index.html',book_shop= 'home', orders = orders)
+
+@shop_app.route('/orders/<index>')
+def order(index):
+    actual_order = orders[int(index) - 1]
+    return render_template('order.html', book_shop= 'order', order = actual_order)
+
